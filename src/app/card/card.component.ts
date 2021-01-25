63b711aac55f2e95d92248d77a1ad7e95ff4e8db
ShabanK <shabankhawar99@gmail.com>
2021-01-25 18:26:10 +0500

    removed additional import

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit, } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -10,7 +10,7 @@ export class CardComponent implements OnInit {
   @Input() cardNumber: string;
   @Input() name: string;
   @Input() expiration: string;
-  
+
   constructor() { }
 
   ngOnInit() {
