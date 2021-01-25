d3beb57f414a9038a5840291076131f2bfd9cf0f
ShabanK <shabankhawar99@gmail.com>
2021-01-25 16:09:50 +0500

    generated an input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { ReactiveFormsModule } from "@angular/forms"
 
 import { AppComponent } from './app.component';
 import { CardFormComponent } from './card-form/card-form.component';
+import { InputComponent } from './input/input.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardFormComponent
+    CardFormComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
